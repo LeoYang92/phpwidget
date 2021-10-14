13837165f7f6c7ea628915b70e7d7d8f1e853edf
louie <louie_gk@qq.com>
2021-10-15 00:19:38 +0800

    拍段远程文件是否存在，working...

--- src/File.php
+++ src/File.php
@@ -11,4 +11,27 @@ class File
         $_string_dir .= '/'.date('Y').'/'.date('m');
         return $_string_dir;
     }
+
+    /**
+     * 判断一个远程文件是否存在
+     * @param $_url
+     * @return boolean
+     */
+    static public function existsRemote($_url)
+    {
+        $_url = trim($_url);
+        if(empty($_url)) return false;
+        $Curl = curl_init($_url);
+        curl_setopt($Curl,CURLOPT_NOBODY, true);
+        curl_setopt($Curl, CURLOPT_CUSTOMREQUEST, 'GET');
+        $Result = curl_exec($Curl);
+        $_result = false;
+        if($Result === true) {
+            $_status_code = curl_getinfo($Curl, CURLINFO_HTTP_CODE);
+            if($_status_code == 200) {
+                $_result = true;
+            }
+        }
+        return $_result;
+    }
 }
