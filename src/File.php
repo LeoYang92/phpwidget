1de972659a5a935b9db8aeb885602942fcd280c7
louie <louie_gk@qq.com>
2021-10-21 20:26:03 +0800

    '生成系统唯一的文件名字'

--- src/File.php
+++ src/File.php
@@ -2,14 +2,31 @@
 namespace Kuyuan\Phpwidget;
 class File
 {
-    static public function stringDir($_dir_name = '')
+    /**
+     * 生成一个文件存储相对路径
+     * @param string $_dir_name 文件名字
+     * @param string $_suffix 文件后缀名字
+     * @return string
+     */
+    static public function stringDir($_dir_name = '',$_suffix = '')
     {
         $_string_dir = '';
         if(empty(!$_dir_name)) {
             $_string_dir = $_dir_name;
         }
         $_string_dir .= '/'.date('Y').'/'.date('m');
+        if(!empty($_suffix)) $_string_dir.= '/'.self::fileName($_suffix);
         return $_string_dir;
+    }
+
+    /**
+     * 生成一个文件名字
+     * @param string $_suffix 文件后缀名字
+     * @return string
+     */
+    static public function fileName($_suffix)
+    {
+        return md5(uniqid(microtime(),true)).'.'.$_suffix;
     }
 
     /**
